4eff6c2c5cadd0d8c0dc89a706cd0250cb1f5e6c
andy <andychatindok@gmail.com>
2018-08-31 12:03:41 +0200

    styling and javascript

---
+++ newg.js
@@ -0,0 +1,28 @@
+function logIn(){
+    document.getElementById("signUp").style.display = "block";
+}
+function off(){
+    document.getElementById("signUp").style.display = "none";
+}
+function pass() {
+    document.getElementById("signUp").style.display = "none";
+    document.getElementById("forgotPassword").style.display ="block";
+        }
+function passOff() {
+    document.getElementById("forgotPassword").style.display ="none";
+}
+function message(){
+    document.getElementById("message").style.display = "block"
+}
+function messageoff(){
+    document.getElementById("message").style.display = "none"
+}
+            // Get the modal
+var m = document.getElementById("signUp");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(click) {
+if (click.target == m) {
+    m.style.display = "none";
+}
+}
